7d7e20e800d64c06431c9941bd695adc88bd332f
tomreidnz <treid.nz@gmail.com>
2024-06-22 13:28:09 -0300

    update text for test deploy

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Home = () => {
   return (
     <main className="flex min-h-screen flex-col">
-      <h1 className="text-2xl">Home page</h1>
+      <h1 className="text-2xl text-orange-500">Home page</h1>
       <Link href="/about">About</Link>
       <Link href="/battle-bank">Battle Bank</Link>
       <Link href="/bravocare">BravoCare</Link>
